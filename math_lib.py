38beef9178248569c42621359058e904ce176be3
owenmadin <owen.madin@colorado.edu>
2019-09-04 16:27:27 -0600

    Cleaned up division function and added documentation, updated readme

--- math_lib.py
+++ math_lib.py
@@ -1,2 +1,23 @@
+import warnings
+
 def div(a, b):
-    return a/b
+    """Divides a number 'a' by another number 'b'
+    
+    Parameters
+    ----------
+        a: int or float
+            Number to be divided
+        b: int or float
+            Number to divide by
+    Returns
+    -------
+        d: int or float
+            Result of the division
+    """
+    
+    if b == 0:
+        warnings.warn('Division by 0 is not possible, returning "None"')
+        d = None
+    else:
+        d = a/b
+    return d

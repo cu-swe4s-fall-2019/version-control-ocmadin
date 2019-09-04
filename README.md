9263cbafd0c347a60dc04c3a4d0dbac019273e7f
owenmadin <owen.madin@colorado.edu>
2019-09-04 12:05:22 -0600

    Added handling of divide-by-0 and function documentation, updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # version_control
 Get familiar with version control basics
+
+# Contents
+
+`math_lib.py`  contains a python division function, updated to handle dividing by 0 and with added documentation 
--- math_lib.py
+++ math_lib.py
@@ -1,2 +1,24 @@
 def div(a, b):
-    return a/b
+    """
+    Divides a number 'a' by another number 'b'
+    
+    Parameters
+    ----------
+    a: int or float
+        Number to be divided
+    b: int or float
+        Number to divide a by
+    
+    Returns
+    -------
+    d: int or float
+        Result of the division between a and b
+    """
+    
+    if b == 0:
+        print('Error: cannot divide by 0')
+        quit()
+    
+    d=a/b
+    
+    return d
